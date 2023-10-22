e0fb74be340f7dd94ce7e5c94bf390fa4f91495b
Rochelle Mullen <b00156311@mytudublin.ie>
2023-10-22 14:44:43 +0100

    start

---
+++ Challenge3_B00156311/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,92 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void MoveLeftX_Start_m252B91087F37116C77F4CB7391F1F40EB52C3F4D (void);
+extern void MoveLeftX_Update_m55602C88C7FD769587EA25057C19A3C02A0EFB37 (void);
+extern void MoveLeftX__ctor_m491F8CE682FBDDAC17B563DB8EEEF18ECADEA883 (void);
+extern void PlayerControllerX_Start_m5BAE410291E1DABAC99D3F233D14AF0AA072A606 (void);
+extern void PlayerControllerX_Update_mE690DFA5B4D44A839AC48EC903E382790B5CCC8B (void);
+extern void PlayerControllerX_OnCollisionEnter_m35E6B0ED27CD27A2652507073E9CD8965D19E32D (void);
+extern void PlayerControllerX__ctor_m3FFA731641AD6FA1CE627820042B7587E98D535A (void);
+extern void RepeatBackgroundX_Start_mCC25F5A1C1CCD6F31A806A028011994E1A810F08 (void);
+extern void RepeatBackgroundX_Update_m20A8C5A0A0C306285D197CB122141F7B74860521 (void);
+extern void RepeatBackgroundX__ctor_m070298E2BCB37A9C02FBF7C9A3BA1EE617C526BC (void);
+extern void SpawnManagerX_Start_mF974670676EF98526100450B651ABED6529E8FD0 (void);
+extern void SpawnManagerX_SpawnObjects_mF21217315F0443F76B98897C4848343BDC5706F1 (void);
+extern void SpawnManagerX__ctor_m967D2E81B63EF3B6BCA2C206459FFAB7EBFFE60E (void);
+extern void SpinObjectsX_Update_m9C5EE349E1608B916F2DF217A48AE96D087956EB (void);
+extern void SpinObjectsX__ctor_m161ED225B7767334CBEBBB7981D66A3DC5AD257B (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[17] = 
+{
+	MoveLeftX_Start_m252B91087F37116C77F4CB7391F1F40EB52C3F4D,
+	MoveLeftX_Update_m55602C88C7FD769587EA25057C19A3C02A0EFB37,
+	MoveLeftX__ctor_m491F8CE682FBDDAC17B563DB8EEEF18ECADEA883,
+	PlayerControllerX_Start_m5BAE410291E1DABAC99D3F233D14AF0AA072A606,
+	PlayerControllerX_Update_mE690DFA5B4D44A839AC48EC903E382790B5CCC8B,
+	PlayerControllerX_OnCollisionEnter_m35E6B0ED27CD27A2652507073E9CD8965D19E32D,
+	PlayerControllerX__ctor_m3FFA731641AD6FA1CE627820042B7587E98D535A,
+	RepeatBackgroundX_Start_mCC25F5A1C1CCD6F31A806A028011994E1A810F08,
+	RepeatBackgroundX_Update_m20A8C5A0A0C306285D197CB122141F7B74860521,
+	RepeatBackgroundX__ctor_m070298E2BCB37A9C02FBF7C9A3BA1EE617C526BC,
+	SpawnManagerX_Start_mF974670676EF98526100450B651ABED6529E8FD0,
+	SpawnManagerX_SpawnObjects_mF21217315F0443F76B98897C4848343BDC5706F1,
+	SpawnManagerX__ctor_m967D2E81B63EF3B6BCA2C206459FFAB7EBFFE60E,
+	SpinObjectsX_Update_m9C5EE349E1608B916F2DF217A48AE96D087956EB,
+	SpinObjectsX__ctor_m161ED225B7767334CBEBBB7981D66A3DC5AD257B,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[17] = 
+{
+	1119,
+	1119,
+	1119,
+	1119,
+	1119,
+	946,
+	1119,
+	1119,
+	1119,
+	1119,
+	1119,
+	1119,
+	1119,
+	1119,
+	1119,
+	2158,
+	1119,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	17,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
